83e4a544d14060035b425fe8e950cde1dced6695
Daniel Trebbien <dtrebbien@gmail.com>
2012-01-02 17:40:23 -0500

    Provide scripts for minifying with YUI Compressor or Closure Compiler
    There are two different Batch scripts. `run_yuicompressor.bat` uses
    YUI Compressor 2.4.7. `run_closure_compiler.bat` uses the latest version
    of Closure Compiler.
    
    The copyright block was moved into `COPYING`.

--- run_yuicompressor.bat
+++ run_yuicompressor.bat
@@ -1,8 +1,7 @@
 @echo off
 mkdir build 2>nul
-del build\BigDecimal-all-1.0.2.js 2>nul
-copy /B src\_begin.js + src\MathContext.js + src\_middle.js + src\BigDecimal.js + src\_end.js build\BigDecimal-all-1.0.2.js >nul
-REM java -jar bin\yuicompressor-2.4.6.jar build\BigDecimal-all-1.0.2.js --charset UTF-8 -o build\BigDecimal-all-1.0.2.min.js
-java -jar \bin\closure_compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js build\BigDecimal-all-1.0.2.js --js_output_file build\BigDecimal-all-1.0.2.min.js
-
-copy build\BigDecimal-all-1.0.2.min.js c:\work\jquery.multiselect\js\libs\BigDecimal-icu4j-1.0.2.min.js
+del build\BigDecimal-all-last.js 2>nul
+copy /B build\preserved_multiline_comment_begin.yui.js + COPYING + build\preserved_multiline_comment_end.js + src\_begin.js + src\MathContext.js + src\_middle.js + src\BigDecimal.js + src\_end.js build\BigDecimal-all-last.js >nul
+echo Created build\BigDecimal-all-last.js
+java -jar bin\yuicompressor-2.4.7.jar build\BigDecimal-all-last.js --charset UTF-8 -o build\BigDecimal-all-last.min.js
+echo Created build\BigDecimal-all-last.min.js
