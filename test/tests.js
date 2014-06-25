9dd04d96a751dccc45273bc8c8b3c299efcc0a4c
Daniel Trebbien <dtrebbien@gmail.com>
2014-06-25 07:16:02 -0400

    Update to latest QUnit

--- test/tests.js
+++ test/tests.js
@@ -6,7 +6,7 @@ var window = this
 , BigDecimal = window.BigDecimal
 , MathContext = window.MathContext
 
-test("Calculate E", function () {
+test("Calculate E", function (assert) {
 	var scale = 20;
 	var e = new BigDecimal("0");
 	var denominator = new BigDecimal("1");
@@ -20,40 +20,40 @@ test("Calculate E", function () {
 		++iteration;
 	}
 
-	equal(e.toString(), "2.71828182845904523536");
+	assert.equal(e.toString(), "2.71828182845904523536");
 });
 
-test("Convenience functions", function() {
-	ok(new BigDecimal("1234.5678").isPositive(), "1234.5678 is positive");
-	ok(new BigDecimal("-1234.5678").isNegative(), "-1234.5678 is negative");
-	ok(!BigDecimal.ZERO.isPositive() &&
-	   !BigDecimal.ZERO.isNegative(), "BigDecimal.ZERO is neither positive nor negative");
-	ok(BigDecimal.ZERO.isZero(), "BigDecimal.ZERO is zero");
-	ok(new BigDecimal("1234.0000").isLessThan(new BigDecimal("1234.5678")) &&
-	   new BigDecimal("1234.0000").isLessThanOrEqualTo(new BigDecimal("1234.5678")), "1234.0000 is less than 1234.5678");
-	ok(!new BigDecimal("1234.0000").isGreaterThan(new BigDecimal("1234.5678")) &&
-	   !new BigDecimal("1234.0000").isGreaterThanOrEqualTo(new BigDecimal("1234.5678")), "1234.0000 is not greater than 1234.5678");
-	ok(new BigDecimal("1234.5678").isGreaterThan(new BigDecimal("1234.0000")) &&
-	   new BigDecimal("1234.5678").isGreaterThanOrEqualTo(new BigDecimal("1234.0000")), "1234.5678 is greater than 1234.0000");
-	ok(!new BigDecimal("1234.5678").isLessThan(new BigDecimal("1234.0000")) &&
-	   !new BigDecimal("1234.5678").isLessThanOrEqualTo(new BigDecimal("1234.0000")), "1234.5678 is not less than 1234.0000");
-	ok(new BigDecimal("1234.5678").isLessThanOrEqualTo(new BigDecimal("1234.5678")) &&
-	   new BigDecimal("1234.5678").isGreaterThanOrEqualTo(new BigDecimal("1234.5678")), "1234.5678 is less than or equal to itself as well as greater than or equal to itself");
+test("Convenience functions", function (assert) {
+	assert.ok(new BigDecimal("1234.5678").isPositive(), "1234.5678 is positive");
+	assert.ok(new BigDecimal("-1234.5678").isNegative(), "-1234.5678 is negative");
+	assert.ok(!BigDecimal.ZERO.isPositive() &&
+	          !BigDecimal.ZERO.isNegative(), "BigDecimal.ZERO is neither positive nor negative");
+	assert.ok(BigDecimal.ZERO.isZero(), "BigDecimal.ZERO is zero");
+	assert.ok(new BigDecimal("1234.0000").isLessThan(new BigDecimal("1234.5678")) &&
+	          new BigDecimal("1234.0000").isLessThanOrEqualTo(new BigDecimal("1234.5678")), "1234.0000 is less than 1234.5678");
+	assert.ok(!new BigDecimal("1234.0000").isGreaterThan(new BigDecimal("1234.5678")) &&
+	          !new BigDecimal("1234.0000").isGreaterThanOrEqualTo(new BigDecimal("1234.5678")), "1234.0000 is not greater than 1234.5678");
+	assert.ok(new BigDecimal("1234.5678").isGreaterThan(new BigDecimal("1234.0000")) &&
+	          new BigDecimal("1234.5678").isGreaterThanOrEqualTo(new BigDecimal("1234.0000")), "1234.5678 is greater than 1234.0000");
+	assert.ok(!new BigDecimal("1234.5678").isLessThan(new BigDecimal("1234.0000")) &&
+	          !new BigDecimal("1234.5678").isLessThanOrEqualTo(new BigDecimal("1234.0000")), "1234.5678 is not less than 1234.0000");
+	assert.ok(new BigDecimal("1234.5678").isLessThanOrEqualTo(new BigDecimal("1234.5678")) &&
+	          new BigDecimal("1234.5678").isGreaterThanOrEqualTo(new BigDecimal("1234.5678")), "1234.5678 is less than or equal to itself as well as greater than or equal to itself");
 });
 
 module("Rounding")
 // What we normally refer to as 'rounding' in 'normal' parlor, in fixed point
 // math apparently is called 'change scale'
 
-test("Rounding by setScale()", function(){
+test("Rounding by setScale()", function (assert) {
 	var a = new BigDecimal('1234.3456000')
 	//expect(4)
-	equal(
+	assert.equal(
 		a.setScale(4).toString()
 		, '1234.3456'
 		, 'Test ROUND_UNNECESSARY'
 	)
-	raises(
+	assert.throws(
 		function () {
 			a.setScale(3);
 		}
@@ -62,7 +62,7 @@ test("Rounding by setScale()", function(){
 		}
 		, 'Test ROUND_UNNECESSARY (2) - Should raise an exception to indicate that a rounding mode must be specified'
 	)
-	raises(
+	assert.throws(
 		function () {
 			a.setScale(1);
 		}
@@ -71,12 +71,12 @@ test("Rounding by setScale()", function(){
 		}
 		, 'Test ROUND_UNNECESSARY (3) - Should raise an exception to indicate that a rounding mode must be specified'
 	)
-	equal(
+	assert.equal(
 		new BigDecimal('1234.00').setScale(0).toString()
 		, '1234'
 		, 'Test ROUND_UNNECESSARY (4)'
 	)
-	equal(
+	assert.equal(
 		a.setScale(3, MathContext.ROUND_HALF_UP).toString()
 		, '1234.346'
 		, 'Test ROUND_HALF_UP'
@@ -85,25 +85,25 @@ test("Rounding by setScale()", function(){
 
 module("Division + Multiplication")
 
-test("Moving the decimal point", function(){
+test("Moving the decimal point", function (assert) {
 	var a = new BigDecimal('1234.5678000')
 	//expect(4)
-	equal(
+	assert.equal(
 		a.movePointRight(2).toString()
 		, a.movePointLeft(-2).toString()
 		, 'Moving the decimal point to the right by 2 is the same as moving it to the left by -2.'
 	)
-	equal(
+	assert.equal(
 		a.movePointRight(-2).toString()
 		, a.movePointLeft(2).toString()
 		, 'Moving the decimal point to the right by -2 is the same as moving it to the left by 2.'
 	)
-	equal(
+	assert.equal(
 		a.movePointRight(10).toString()
 		, "12345678000000"
 		, 'Moving the decimal point to the right by 10 is the same as multiplying by 10^10.'
 	)
-	equal(
+	assert.equal(
 		a.movePointRight(-10).toString()
 		, "0.00000012345678000"
 		, 'Moving the decimal point to the right by -10 is the same as dividing by 10^10.'
